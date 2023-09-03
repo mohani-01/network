5764a0036945df94c05076c5c0fac7421b61d3ef
Nahom <nahomohani@gmail.com>
2023-09-03 08:28:26 +0300

    not working

--- network/models.py
+++ network/models.py
@@ -4,17 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    comment = models.TextField()
+    time = models.DateField()
+
+    
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='poster')
     post = models.TextField()
     date = models.DateField(auto_now_add=True)
     like = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
+    comment = models.ManyToManyField(Comment, blank=True, related_name="post")
 
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
-    comment = models.TextField()
-    time = models.DateField()
-    posts = models.ManyToManyField(Post, blank=True, related_name="comment")
-    
