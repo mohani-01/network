1bb5eff1efe234e3d27d8cabb7198eb3c352cd4c
Nahom <nahomohani@gmail.com>
2023-09-03 07:42:40 +0300

    starting again

--- network/models.py
+++ network/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='poster')
+    post = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    like = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    comment = models.TextField()
+    time = models.DateField()
+    posts = models.ManyToManyField(Post, blank=True, related_name="comment")
+    
