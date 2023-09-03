f30edcbadddc4a1c1824ae1a508b0c3d972f78da
Nahom <nahomohani@gmail.com>
2023-09-03 07:54:22 +0300

    Transfering completed

--- network/views.py
+++ network/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Post, Comment
+from .forms import NewPost
 
 def index(request):
-    return render(request, "network/index.html")
+    posts = Post.objects.all().order_by('-date')
+    
+
+    return render(request, "network/index.html", {
+        "new_post": NewPost(),
+        "posts": posts,
+    })
 
 
 def login_view(request):
@@ -61,3 +68,21 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+@login_required(login_url="/login")
+def addpost(request):
+
+    if request.method == "POST":
+        form = NewPost(request.POST)
+
+
+        if form.is_valid():
+            post = form.cleaned_data["post"]
+            print(post)
+            Post.objects.create(user=request.user, post=post)
+    
+
+        print(request.user)
+
+        return HttpResponseRedirect(reverse('index'))
